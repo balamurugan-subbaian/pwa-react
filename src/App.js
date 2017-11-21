41600d6cc658616580336a009bdd74c113805bc5
balamurugan-subbaian <balamurugan.s@highpeaksw.com>
2017-11-21 17:28:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import './App.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <Link to="/"> Feed </Link>
+    <Link to="/profile">Profile</Link>
+  </div>
+);
+
+const Template = ({title}) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      this is the {title} page.
+    </p>
+  </div>
+);
+
+const Feed = (props) =>(
+  <Template title="Feed" />
+);
+
+const Profile = (props) =>(
+  <Template title="profile"/>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Feed}/>
+          <Route path="/profile" component={Profile}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
